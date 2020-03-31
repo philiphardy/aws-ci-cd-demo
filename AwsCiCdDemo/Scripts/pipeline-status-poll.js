d5f36c5921d3e34462cc6f9e13d2d4de6dd7c788
AwsCiCdDemoBot <philiphardy4444@gmail.com>
2020-03-31 08:19:15 -0700

    Added pipeline status label

--- AwsCiCdDemo/Scripts/pipeline-status-poll.js
+++ AwsCiCdDemo/Scripts/pipeline-status-poll.js
@@ -1,4 +1,9 @@
 ﻿const pipelineInfoContainer = $('#pipeline-info-container');
+const pipelineStatus = $('#pipeline-status');
+
+function isPipelineRunning(data) {
+    return !!data.StageStates.find(stageState => stageState.LatestExecution.Status.Value === 'InProgress');
+}
 
 function getPipelineInfo() {
     $.getJSON('/api/pipeline')
@@ -6,10 +11,20 @@ function getPipelineInfo() {
             if (!data) {
                 return;
             }
+
+            if (isPipelineRunning(data)) {
+                pipelineStatus.text("Running");
+                pipelineStatus.addClass("label-success");
+                pipelineStatus.removeClass("label-default");
+            } else {
+                pipelineStatus.text("Idle");
+                pipelineStatus.addClass("label-default");
+                pipelineStatus.removeClass("label-success");
+            }
+
             const json = JSON.stringify(data, null, 3)
                 .replace(/ /g, '&nbsp;')
                 .replace(/\n/g, '<br/>');
-            console.log(json);
             pipelineInfoContainer.html(json);
         })
         .fail((xhr, err) => console.error('failed to get pipeline status', err));
